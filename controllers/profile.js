787109cf5aee715498a6e332b0fd53301d28fc3a
kellenwiltshire <kellenwiltshire@outlook.com>
2021-01-18 15:43:37 -0500

    Updated Server to be Cleaner

---
+++ controllers/profile.js
@@ -0,0 +1,31 @@
+const handleProfileGet = (req, res, db) => {
+    const { id } = req.params;
+    db.select('*').from('users')
+    .where({id})
+    .then(user => {
+        if(user.length){
+            res.json(user[0])
+        } else {
+            res.status(400).json('not found')
+        }
+    })
+    .catch(err => res.status(400).json('Error getting user'))
+}
+
+const handleProfileDelete = (req, res, db) => {
+    const { id, email } = req.body;
+    db.select('*').from('users')
+    .where({id})
+    .del()
+    .catch(err => res.status(400).json('Error Deleting User'))
+
+    db.select('*').from('login')
+    .where({email})
+    .del()
+    .catch(err => res.status(400).json('Error Deleting User'))
+}
+
+module.exports = {
+    handleProfileGet: handleProfileGet,
+    handleProfileDelete: handleProfileDelete
+}
--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const cors = require('cors');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-const knex = require('knex')
+const knex = require('knex');
+
+const register = require('./controllers/register.js');
+const signin = require('./controllers/signin.js');
+const profile = require('./controllers/profile.js')
+const image = require('./controllers/image.js')
 
 const db = knex({
     client: 'pg',
@@ -26,93 +31,18 @@ app.get('/', (req, res) => {
 })
 
 //! SIGN IN FUNTION
-app.post('/signin', (req, res) => {
-    db.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
-        .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
-            if(isValid) {
-                return db.select('*').from('users')
-                    .where('email', '=', req.body.email)
-                    .then(user => {
-                        res.json(user[0])
-                    })
-                    .catch(err => res.status(400).json('unable to get user'))
-            } else {
-                res.status(400).json('wrong credentials');
-            } 
-        })
-        .catch(err => res.status(400).json('wrong credentials'))
-})
+app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) })
 
 //! REGISTER FUNCTION
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password, saltRounds);
-        db.transaction(trx => {
-            trx.insert({
-                hash: hash,
-                email: email
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return trx('users')
-                    .returning('*')
-                    .insert({
-                        email: loginEmail[0],
-                        name: name,
-                        joined: new Date()
-                    })
-                    .then(user => {
-                        res.json(user[0]) 
-                    })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-        })
-        .catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt, saltRounds) })
 
-//! PROFILE FUNTION
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users')
-    .where({id})
-    .then(user => {
-        if(user.length){
-            res.json(user[0])
-        } else {
-            res.status(400).json('not found')
-        }
-    })
-    .catch(err => res.status(400).json('Error getting user'))
-})
+//! PROFILE FUNTIONS
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 
-app.delete('/profile/:id', (req, res) => {
-    const { id, email } = req.body;
-    db.select('*').from('users')
-    .where({id})
-    .del()
-    .catch(err => res.status(400).json('Error Deleting User'))
-
-    db.select('*').from('login')
-    .where({email})
-    .del()
-    .catch(err => res.status(400).json('Error Deleting User'))
-})
+app.delete('/profile/:id', (req, res) => { profile.handleProfileDelete(req, res, db) })
 
 //!IMAGE FUNCTION
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries => {
-            res.json(entries[0])
-        })
-        .catch(err => res.status(400).json('Unable to get entries'))
-})
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.listen(3000, () => {
     console.log('app is running on port 3000')
