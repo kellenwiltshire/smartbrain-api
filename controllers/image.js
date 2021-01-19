8f8d39aa902a7bbc18b574c37448511ddfae1a0f
kellenwiltshire <kellenwiltshire@outlook.com>
2021-01-19 15:52:17 -0500

    minor formatting changes

--- controllers/image.js
+++ controllers/image.js
@@ -4,30 +4,31 @@ const api = require('../api/api');
 const apiKey = api.api();
 
 const app = new Clarifai.App({
-    apiKey: apiKey
+	apiKey: apiKey,
 });
 
 const handleApiCall = (req, res) => {
-    app.models
-    .predict(Clarifai.CELEBRITY_MODEL, req.body.input)
-    .then(data => {
-        res.json(data);
-    })
-    .catch(err => res.status(400).json('unable to work with api'))
-}
+	app.models
+		.predict(Clarifai.CELEBRITY_MODEL, req.body.input)
+		.then((data) => {
+			res.json(data);
+		})
+		.catch((err) => res.status(400).json('unable to work with api'));
+};
 
 const handleImage = (req, res, db) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries => {
-            res.json(entries[0])
-        })
-        .catch(err => res.status(400).json('Unable to get entries'))
-}
+	const { id } = req.body;
+	db('users')
+		.where('id', '=', id)
+		.increment('entries', 1)
+		.returning('entries')
+		.then((entries) => {
+			res.json(entries[0]);
+		})
+		.catch((err) => res.status(400).json('Unable to get entries'));
+};
 
 module.exports = {
-    handleImage: handleImage,
-    handleApiCall: handleApiCall
-}
+	handleImage: handleImage,
+	handleApiCall: handleApiCall,
+};
